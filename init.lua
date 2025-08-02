eb91c3330d48f73f0de409597d78e78ba38c4e20
Ely <>
2025-08-02 14:34:45 +0300

    more mini plugins including mini clue

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 vim.opt.clipboard = "unnamedplus"
 require("config.lazy")
+require("config.mini_clue")
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,5 +3,10 @@ return {
 	version = false,
 	config = function()
 		require("mini.pairs").setup()
+		require("mini.comment").setup()
+		require('mini.completion').setup()
+		require('mini.move').setup()
+		require('mini.surround').setup()
+		require('mini.bracketed').setup()
 	end,
 }
