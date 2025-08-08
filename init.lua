5a636eacd4878a13ac9218d82ddee6ba8b5b6288
Ely <>
2025-08-08 15:36:38 +0300

    Added relative line numbers and reload command to reload configuration

--- init.lua
+++ init.lua
@@ -3,8 +3,12 @@ require("config.mini_clue")
 require("config.mini_files")
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.o.timeoutlen = 2000
+
 vim.keymap.set("n", "-", function()
 	MiniExtra.pickers.explorer()
 end)
 
+vim.api.nvim_create_user_command("Reload", "source $MYVIMRC", {})
