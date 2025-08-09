350791218520cc29891996d7fc771f814481ca3f
Ely <>
2025-08-09 20:36:21 +0300

    Rust-analyzer+syntax+rustfmt working, no debug/compile

--- init.lua
+++ init.lua
@@ -5,6 +5,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.o.timeoutlen = 2000
+vim.lsp.inlay_hint.enable()
+vim.diagnostic.config({ virtual_text = true })
+vim.g.rustfmt_fail_silently = 1
 
 vim.keymap.set("n", "-", function()
 	MiniExtra.pickers.explorer()
@@ -20,3 +23,4 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 
 MiniMisc.setup_auto_root()
 MiniMisc.setup_restore_cursor()
+
